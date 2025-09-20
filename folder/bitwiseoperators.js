21a4ab62382211b6ce6efa454174640021253e98
venkatguvi <venkatagopichand9@gmail.com>
2025-09-20 10:39:18 +0530

    added the folder test

---
+++ folder/bitwiseoperators.js
@@ -0,0 +1,7 @@
+
+let a = 5; // 0101
+let b = 3; //0011
+
+// bitwsie operator 
+console.log(a & b);
+
---
+++ folder/comparisonoperator.js
@@ -0,0 +1,29 @@
+let price = 100;
+let discount = "100";
+
+// equal....it is going to check  only value, boolean value
+console.log(price == discount);
+
+
+//strict equal or equality...it is going to check both value and data type
+console.log(price === discount); //false
+
+//not equal
+console.log(price !=50); //true
+
+//strict not equal
+console.log(100 !== "100"); 
+
+//greater than
+console.log(price > 50); //true
+
+
+//less than
+console.log(price < 50); //false
+
+//greaten than or equal
+console.log(price >= 100); //true
+
+
+//less than or equal
+console.log(price <= 150); //true
